a60dbeb65c8e3b45b509afc75240d9ca0769d6cc
dsaez27 <dssh27@gmail.com>
2021-01-01 17:44:43 -0300

    03-useFetch

--- 05-hook-app/src/components/03-useFetch/MultipleCustomHooks.js
+++ 05-hook-app/src/components/03-useFetch/MultipleCustomHooks.js
@@ -1,27 +1,35 @@
 import React from 'react';
+import { useCounter } from '../../Hooks/useCounter';
 import { useFetch } from '../../Hooks/useFetch';
+
 import '../02-useEffect/effects.css';
 
 export const MultipleCustomHooks = () => {
+	const { counter, increment } = useCounter(1);
 
-    const { loading, data } = useFetch(`https://www.breakingbadapi.com/api/quotes/1`);
-    
-    const { author, quote }  = !!data && data[0];
-    console.log(author, quote);
+	const { loading, data } = useFetch(
+		`https://www.breakingbadapi.com/api/quotes/${counter}`
+	);
 
-    return (
+	const { author, quote } = !!data && data[0];
+
+	return (
 		<>
 			<h1>Breaking Hooks</h1>
 			<hr />
 
-            {
-                loading ? (<div className='alert alert-info text-center'>Cargando...</div>) : (
-                    <blockquote className='blockquote text-end'>
-                        <p className=''> { quote }</p>
-                        <footer className='blockquote-footer'> { author } </footer>
-                    </blockquote>
-                )
-            }
+			{loading ? (
+				<div className='alert alert-info text-center'>Cargando...</div>
+			) : (
+				<blockquote className='blockquote text-end'>
+					<p className=''> {quote}</p>
+					<footer className='blockquote-footer'> {author} </footer>
+				</blockquote>
+			)}
+
+			<button className='btn btn-primary' onClick={increment}>
+				Siguiente quote
+			</button>
 		</>
 	);
-}
+};
