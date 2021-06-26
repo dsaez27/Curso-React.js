b60950e2a4d2cacd7a647e0a131e92e7e7bfb29f
dsaez27 <dssh27@gmail.com>
2021-06-26 15:46:06 -0400

    useContext TodoApp Complete

--- 05-hook-app/src/components/09-useContext/AppRouter.js
+++ 05-hook-app/src/components/09-useContext/AppRouter.js
@@ -1,22 +1,24 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
+import { NavBar } from './NavBar';
 import { LoginScreen } from './LoginScreen';
 import { HomeScreen } from './HomeScreen';
 import { AboutScreen } from './AboutScreen';
-import { NavBar } from './NavBar';
 
 export const AppRouter = () => {
     return (
         <Router>
             <div>
                 <NavBar />
-                <Switch>
-                    <Route path="/" exact component={HomeScreen} />
-                    <Route path="/about" exact component={AboutScreen} />
-                    <Route path="/login" exact component={LoginScreen} />
-                    <Redirect to="/" />
-                </Switch>
+                <div className="container">
+                    <Switch>
+                        <Route path="/" exact component={HomeScreen} />
+                        <Route path="/about" exact component={AboutScreen} />
+                        <Route path="/login" exact component={LoginScreen} />
+                        <Redirect to="/" />
+                    </Switch>
+                </div>
             </div>
         </Router>
-    )
-}
+    );
+};
--- 05-hook-app/src/components/09-useContext/LoginScreen.js
+++ 05-hook-app/src/components/09-useContext/LoginScreen.js
@@ -1,10 +1,24 @@
-import React from 'react'
+import React, { useContext } from 'react';
+import { UserContext } from './UserContext';
 
 export const LoginScreen = () => {
+    const { setUser } = useContext(UserContext);
+
+    const handleLogin = () => {
+        setUser({
+            id: 123,
+            name: 'Daniel',
+            email: 'dssh27@gmail.com'
+        })
+    }
+
     return (
         <>
             <h1>LoginScreen</h1>
             <hr />
+            <button className='btn btn-primary' onClick={handleLogin}>
+                Login
+            </button>
         </>
     );
-}
+};
