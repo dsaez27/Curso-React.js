064c299322af7459c2c7377acd705d584fe44e65
dsaez27 <dssh27@gmail.com>
2021-06-26 03:13:40 -0400

    useReducer - TodoApp

--- 05-hook-app/src/components/08-useReducer/TodoApp.js
+++ 05-hook-app/src/components/08-useReducer/TodoApp.js
@@ -1,26 +1,58 @@
-import React, { useReducer } from 'react';
-import './style.css';
+import React, { useEffect, useReducer } from 'react';
 import { todoReducer } from './todoReducer';
+import { TodoList } from './TodoList';
+import { TodoAdd } from './TodoAdd';
 
-const initialState = [{
-    id: new Date().getTime(),
-    desc: 'Aprender React',
-    done: false
-}]
+import './style.css';
+
+const init = () => {
+    return JSON.parse(localStorage.getItem('todos')) || [];
+    /* return [
+    {
+        id: new Date().getTime(),
+        desc: 'Aprender React',
+        done: false,
+    }, 
+]*/
+};
 
 export const TodoApp = () => {
-    const [state] = useReducer(todoReducer, initialState);
+    const [todos, dispatch] = useReducer(todoReducer, [], init);
+
+    useEffect(() => {
+        localStorage.setItem('todos', JSON.stringify(todos));
+    }, [todos]);
+
+    const handleAddTodo = ( newTodo) => {
+        dispatch({type: 'add', payload: newTodo});
+    }
+
+    const handleDelete = (todoId) => {
+        dispatch({ type: 'delete', payload: todoId });
+    };
+
+    const handleToggle = (todoId) => {
+        dispatch({ type: 'toggle', payload: todoId });
+    };
 
     return (
         <>
-            <h1>TodoApp</h1>
+            <h1>TodoApp ({todos.length})</h1>
             <hr />
 
-            <ul>
-                <li>Hola</li>
-                <li>Mundo</li>
-                <li>Hola mundo de nuevo</li>
-            </ul>
+            <div className="row">
+                <div className="col-7">
+                    {/* TodoList, todos ,handleDelete, handleToggle*/}
+                    <TodoList
+                        todos={todos}
+                        handleDelete={handleDelete}
+                        handleToggle={handleToggle}
+                    />
+                </div>
+                <div className="col-5">
+                   <TodoAdd handleAddTodo={handleAddTodo} />
+                </div>
+            </div>
         </>
     );
 };
--- 05-hook-app/src/index.js
+++ 05-hook-app/src/index.js
@@ -1,11 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-<<<<<<< HEAD
 import { TodoApp } from './components/08-useReducer/TodoApp';
-=======
-import { Padre } from './Components/07-tarea-memo/Padre';
-
-ReactDOM.render(<Padre />, document.getElementById('root'));
->>>>>>> 1c411d669d8b8a7a4bfd1a376c980bac5880408a
 
 ReactDOM.render(<TodoApp />, document.getElementById('root'));
